27b27283d55573f5db58977163b556a55468287e
prozet <abcd-xyz@list.ru>
2022-10-21 00:10:29 +0700

    st 1

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+/*require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 
@@ -10,4 +10,30 @@ $app->addErrorMiddleware(true, true, true);
 $app->get('/', function ($req, $resp) {
     return $resp->write('Welcome to Slim!');
 });
+$app->run();*/
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Middleware\MethodOverrideMiddleware;
+
+session_start();
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
+
+$router = $app->getRouteCollector()->getRouteParser();
+
+$app->get('/', function ($req, $resp) {
+    return $resp->write('Welcome to Slim!');
+});
+
 $app->run();
