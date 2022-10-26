7422bfd216b1a64b6ca877dedf2f49bb9d21108c
Dmitriy Nesterovich <abcd-xyz@list.ru>
2022-10-26 21:23:07 +0700

    st 3

--- public/index.php
+++ public/index.php
@@ -32,6 +32,59 @@ $app->get('/', function ($req, $resp) {
     return $this -> get('renderer') -> render($resp, 'main.phtml', $params);
 }) -> setName('main');
 
+$app->get('/urls', function ($req, $resp) use ($router) {
+    try {
+        $connection = getconnectionToDB();
+    }
+    catch (Exception $e) {
+        $this -> get('flash') -> addMessage('error', 'Возникла внутренняя ошибка сервера. Попробуйте выполнить действие позже.');
+        return $resp -> withRedirect($router -> urlFor('main'), 302);
+    }
+
+    $queryForUrls = "SELECT id, name FROM urls ORDER BY id DESC";
+    $resQueryForUrls = $connection -> query($queryForUrls);
+    $urls = [];
+    foreach ($resQueryForUrls as $row) {
+        $urls[] = ['id' => $row['id'], 'name' => $row['name']];
+    }
+    $params = ['urls' => $urls];
+    return $this -> get('renderer') -> render($resp, 'urls.phtml', $params);
+}) -> setName('urls');
+
+$app->get('/urls/{id}', function ($req, $resp, $args) use ($router) {
+    try {
+        $connection = getconnectionToDB();
+    }
+    catch (Exception $e) {
+        $this -> get('flash') -> addMessage('error', 'Возникла внутренняя ошибка сервера. Попробуйте выполнить действие позже.');
+        return $resp -> withRedirect($router -> urlFor('main'), 302);
+    }
+
+    $id = $args['id'];
+    $queryForUrl = "SELECT * FROM urls WHERE id={$id}";
+    $resQueryForUrl = $connection -> query($queryForUrl);
+    $urlData = $resQueryForUrl -> fetch();
+    $dateTime = new DateTime($urlData['created_at']);
+    $dateTimeFormatted = $dateTime->format('Y-m-d H:i:s');
+    $params = ['url' => ['id' => $id, 'name' => $urlData['name'], 'created_at' => $dateTimeFormatted]];
+    return $this -> get('renderer') -> render($resp, 'url.phtml', $params);
+}) -> setName('urlID');
+
+$app -> post('/clearurls', function ($req, $resp) use ($router) {
+    try {
+        $connection = getconnectionToDB();
+    }
+    catch (Exception $e) {
+        $this -> get('flash') -> addMessage('error', 'Возникла внутренняя ошибка сервера. Попробуйте выполнить действие позже.');
+        return $resp -> withRedirect($router -> urlFor('main'), 302);
+    }
+
+    $queryForClearing = "DELETE FROM urls";
+    $connection->query($queryForClearing);
+    $this -> get('flash') -> addMessage('warning', 'Таблица urls очищена');
+    return $resp -> withRedirect($router -> urlFor('main'), 302);
+});
+
 $app -> post('/urls', function($req, $resp) use ($router) {
     $inputedURL = $req -> getParsedBodyParam('url', null);
 
@@ -40,47 +93,60 @@ $app -> post('/urls', function($req, $resp) use ($router) {
         return $resp -> withRedirect($router -> urlFor('main'));
     }
 
-    $url = parse_url($inputedURL['name']);
-    $scheme = $url['scheme'];
-    $host = $url['host'];
-    $validator = new Valitron\Validator(array('url' => $inputedURL['name'], 'host' => $host));
+    $url = $inputedURL['name'];
+    $urlParsed = parse_url($url);
+    $scheme = $urlParsed['scheme'];
+    $host = $urlParsed['host'];
+    $validator = new Valitron\Validator(array('url' => $url, 'host' => $host));
     $validator -> rule('required', 'url')
         -> rule('lengthMax', 'url', 255)
         -> rule('url', 'url')
         -> rule('contains', 'host', '.');
-    if($validator->validate()) {
-        $connectionString = "pgsql:host=localhost;port=5432;dbname=phpproj3test;";
-        /*try {
 
-        }
-        catch () {
-
-        }*/
-        $pdo = new PDO($connectionString, 'prozex', 'pwd', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-        if ($pdo) {
-            $resOfQuery = $pdo->query("SELECT COUNT(*) AS counts FROM urls WHERE name='{$scheme}://{$host}'");
-            if (($resOfQuery -> fetch())['counts'] === 0) {
-                $pdo->query("INSERT INTO urls (name, created_at) VALUES ('{$scheme}://{$host}', current_timestamp)");
-            } else {
-                return $resp -> write('Already exists!!!');
-            }
-
-            $res = '';
-            foreach($pdo->query('SELECT * from urls') as $row) {
-                $res .= $row['name'] . '   ' . $row['created_at'];
-            }
-            return $resp -> write($res);
-        }
-    } else {
+    if (!($validator->validate())) {
         return $this -> get('renderer') -> render($resp, 'main.phtml', ['badURL' => true, 'inputedURL' => $inputedURL['name']]);
     }
+
+    try {
+        $connection = getconnectionToDB();
+    }
+    catch (Exception $e) {
+        $this -> get('flash') -> addMessage('error', 'Возникла внутренняя ошибка сервера. Попробуйте выполнить действие позже.');
+        return $resp -> withRedirect($router -> urlFor('main'), 302);
+    }
+
+    $queryForExisting = "SELECT COUNT(*) AS counts FROM urls WHERE name='{$scheme}://{$host}'";
+    $resOfQueryForExisting = $connection->query($queryForExisting);
+    if (($resOfQueryForExisting -> fetch())['counts'] === 0) {
+        $queryForInsertNewData = "INSERT INTO urls (name, created_at) VALUES ('{$scheme}://{$host}', current_timestamp)";
+        $connection->query($queryForInsertNewData);
+        $this -> get('flash') -> addMessage('success', 'Страница успешно добавлена');
+    } else {
+        $this -> get('flash') -> addMessage('warning', 'Страница уже существует');
+    }
+    return $resp -> withRedirect($router -> urlFor('main'), 302);
+    /*$res = '';
+    foreach($connection->query('SELECT * from urls') as $row) {
+        $res .= $row['name'] . '   ' . $row['created_at'];
+    }
+    return $resp -> write($res);*/
+
+
 
 
 
     //INSTALL PGSQL-provider: apt-get install php-pgsql. Then restart appache. And you will need to create a pass for user
-
-    return $resp -> write(count(PDO::getAvailableDrivers()));
 });
+
+function getconnectionToDB() {
+    $dbDriver = 'pgsql';
+    $dbHost = 'localhost';
+    $dbPort = '5432';
+    $dbName = 'phpproj3test';
+    $dbUserName = 'prozex';
+    $dbUserPassword = 'pwd';
+    $connectionString = "{$dbDriver}:host={$dbHost};port={$dbPort};dbname={$dbName};";
+    return new PDO($connectionString, $dbUserName, $dbUserPassword, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+}
 
 $app->run();
