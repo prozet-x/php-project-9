5399e26eba58ddf0bec59a206f5fdc07440007e7
prozet <abcd-xyz@list.ru>
2022-11-18 01:03:52 +0700

    st 6

--- public/index.php
+++ public/index.php
@@ -152,7 +152,7 @@ $app -> post('/urls', function ($req, $resp) use ($router) {
     }
 
     $this -> get('flash') -> addMessage('warning', 'Страница уже существует');
-    return $resp -> withStatus(409) -> withRedirect($router->urlFor('urlID', ['id' => $existingId]));
+    return $resp -> withStatus(422) -> withRedirect($router->urlFor('urlID', ['id' => $existingId]));
     //INSTALL PGSQL-provider: apt-get install php-pgsql.
     //Then restart appache. And you will need to create a pass for user
 });
